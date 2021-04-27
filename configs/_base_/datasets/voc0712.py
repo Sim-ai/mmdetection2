6547a90badc27f4e640bbaf9f90eab2ad612109e
Sim-ai <71258346+Sim-ai@users.noreply.github.com>
2021-04-27 17:12:11 +0800

    Update voc0712.py

--- configs/_base_/datasets/voc0712.py
+++ configs/_base_/datasets/voc0712.py
@@ -1,6 +1,6 @@
 # dataset settings
 dataset_type = 'VOCDataset'
-data_root = 'data/VOCdevkit/'
+data_root = '/xiaopeng/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
@@ -47,7 +47,7 @@ data = dict(
         dataset=dict(
             type=dataset_type,
             ann_file=[
-                data_root + 'VOC2007/ImageSets/Main/trainval.txt',
+                data_root + 'VOC2007/ImageSets/Main/train.txt',
 #                 data_root + 'VOC2012/ImageSets/Main/trainval.txt'
             ],
 #             img_prefix=[data_root + 'VOC2007/', data_root + 'VOC2012/'],
